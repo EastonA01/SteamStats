9c6771044c335d27bad92fcb7de6a356da4d4c5e
EastonA01 <eastonredwing@gmail.com>
2023-03-11 18:03:15 -0500

    Added Steam Friends Functionality in /friend route

--- SteamStats_be/hub/views.py
+++ SteamStats_be/hub/views.py
@@ -21,42 +21,81 @@ def user_friends(request, steam_id):
     if request.method == 'GET':
         # if steam_id == "":
         #     return Response(status=status.HTTP_400_BAD_REQUEST)
+        mergedData = []
+        # TODO Eventual mash of data between Set1 (User Friendslist) and Set2 (DisplayNames)
+        # i.e data = data1 + data2
+        data1 = get_friends(steam_id)
 
-    
-        url = f'{api_endpoint}/ISteamUser/GetFriendList/v0001/?key={api_key}&steamid={steam_id}&relationship=friend'
-        req = r.Request(url)
-        req.add_header('User-Agent',"cheese")
-        try:
-            with r.urlopen(req) as response:
-                content = response.read()
+        data2 = display_scraper(data1)
 
-            data = json.loads(content)
-        except:
-            data = "No Data Found..."
+        # TODO Call get_user_summary in conjunction with above function's NEW CSV and store data
+        for i in data1['friendslist']['friends']:
+            tmpEntry = {
+                "steamid": i['steamid'],
+                "friend_since": i['friend_since'],
+            }
+            # print(data2)
+            tmpEntry2 = findUser(i['steamid'], data2["response"]["players"])
+            tmpEntry.update(tmpEntry2)
+            mergedData.append(tmpEntry)
+        # Return set data beneath
 
-        # data={"you_want":steam_id}
-        return Response(status=status.HTTP_200_OK, data=data)
-    
+        return Response(status=status.HTTP_200_OK, data=mergedData)
+
+
 @api_view(['GET'])
 def user_summary(request, steam_id):
     if request.method == 'GET':
         # if steam_id == "":
         #     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    
-        url = f'{api_endpoint}/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_id}'
-        req = r.Request(url)
-        req.add_header('User-Agent',"cheese")
-        try:
-            with r.urlopen(req) as response:
-                content = response.read()
+        data = get_user_summary(steam_id)
 
-            data = json.loads(content)
-        except:
-            data = "No Data Found..."
-
-        # data={"you_want":steam_id}
         return Response(status=status.HTTP_200_OK, data=data)
+
+
+def get_friends(steam_id):
+
+    url = f'{api_endpoint}/ISteamUser/GetFriendList/v0001/?key={api_key}&steamid={steam_id}&relationship=friend'
+    req = r.Request(url)
+    req.add_header('User-Agent', "cheese")
+    try:
+        with r.urlopen(req) as response:
+            content = response.read()
+
+        data = json.loads(content)
+    except:
+        data = "No Data Found..."
+
+    return data
+
+
+def get_user_summary(steam_id):
+
+    url = f'{api_endpoint}/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_id}'
+    req = r.Request(url)
+    req.add_header('User-Agent', "cheese")
+    try:
+        with r.urlopen(req) as response:
+            content = response.read()
+
+        data = json.loads(content)
+    except:
+        data = "No Data Found..."
+
+    return data
+
+
+def display_scraper(response_data):
+    # Loop through i in steam_id
+    ids = ''
+    for i in response_data['friendslist']['friends']:
+        ids += i['steamid']+','
+    return get_user_summary(ids)
+
+
+def findUser(id, lst):
+    return next((i for i in lst if i['steamid'] == id), None)
 
 
 class HubView(viewsets.ModelViewSet):
--- frontend/src/pages/Friend.js
+++ frontend/src/pages/Friend.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 import { API_RAW } from "../constants";
+import moment from 'moment'
 
 export default function Friend() {
     const [friend, setFriend] = useState([])
@@ -11,14 +12,34 @@ export default function Friend() {
     }, [])
 
     function getFriend() {
-        axios.get(API_RAW + "friends/76561198152156381/").then(res => setFriend(res.data));
+        axios.get(API_RAW + "friends/76561198152156381/")
+            .then(res => {
+                let tempList = []
+                res.data.forEach(
+                    i => {
+                        tempList.push(<tr>
+                            <td><img src={i.avatarfull} /></td>
+                            <td>{i.personaname}</td>
+                            <td>{i.steamid}</td>
+                            <td>{moment.unix(i.friend_since).format('MMMM Do YYYY, h:mm:ss a')}</td>
+                        </tr>)
+                    }
+                )
+                setFriend(tempList)
+            });
     }
 
 
 
     return (
         <div className="App">
-            <h3>{JSON.stringify(friend)}</h3>
+            <table>
+                <th></th>
+                <th>Username</th>
+                <th>SteamID</th>
+                <th>Friends Since</th>
+                {friend}
+            </table>
         </div>
     )
 
